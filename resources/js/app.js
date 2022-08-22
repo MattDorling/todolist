453b0984ad660ef25061a14ba280d228639b218a
MattDorling <dorling.mj@gmail.com>
2022-08-22 11:23:18 +0100

    Configure TailwindCSS and Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import './bootstrap'
+import '../css/app.css'
+
+import {createApp} from 'vue'
+
+import App from './Components/App.vue'
+
+createApp(App).mount("#app")
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./resources/**/*.{blade.php,vue,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
