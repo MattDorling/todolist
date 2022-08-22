7475fc4a51eaff6306e8074e52b44d3694fd486c
MattDorling <dorling.mj@gmail.com>
2022-08-22 15:29:24 +0100

    Remove commented lines

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('todos', 'TodoController@index');
-// Route::get('todos/{todo}', 'TodoController@show');
-// Route::post('todos', 'TodoController@store');
-// Route::put('todos/{todo}', 'TodoController@update');
-// Route::delete('todos/{todo}', 'TodoController@delete');
 Route::resource('todos', TodoController::class);
